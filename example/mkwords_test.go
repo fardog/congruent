d515b7c18742fb4ca0586151db80ba613e3a750e
fardog <nate@milkandtang.com>
2016-07-30 17:19:51 -0700

    update mkwords example

--- example/mkwords_test.go
+++ example/mkwords_test.go
@@ -1,12 +1,19 @@
 package example
 
 import (
+	"encoding/json"
 	"testing"
 
 	"github.com/fardog/congruent"
 )
 
 var servers congruent.Servers
+
+type GenerateResponse struct {
+	Ok             bool     `json:"ok"`
+	Result         []string `json:"result"`
+	CandidateCount int      `json:"candidate-count"`
+}
 
 func init() {
 	servers = []*congruent.Server{
@@ -25,6 +32,67 @@ func TestGenerate(t *testing.T) {
 
 	if err := responses.StatusEqual(200); err != nil {
 		t.Error(err)
+	}
+
+	for _, resp := range responses {
+		body := GenerateResponse{}
+		if err := json.Unmarshal(resp.Body, &body); err != nil {
+			t.Error(err)
+			t.Fail()
+		}
+
+		if l := len(body.Result); l != 4 {
+			t.Errorf("Expected 4 results, got %v", l)
+		}
+
+		if !body.Ok {
+			t.Errorf("Expected OK to be true, got %v", body.Ok)
+		}
+
+		if body.CandidateCount != 70806 {
+			t.Errorf(
+				"Expected candidate_count to be 70806, got %v",
+				body.CandidateCount)
+		}
+	}
+}
+
+func TestGenerateOptions(t *testing.T) {
+	request := congruent.NewRequest(
+		"GET",
+		"generate?min-chars=10&max-chars=20&num-words=10",
+		nil,
+		nil)
+
+	responses, err := servers.Request(request)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if err := responses.StatusEqual(200); err != nil {
+		t.Error(err)
+	}
+
+	for _, resp := range responses {
+		body := GenerateResponse{}
+		if err := json.Unmarshal(resp.Body, &body); err != nil {
+			t.Error(err)
+			t.Fail()
+		}
+
+		if l := len(body.Result); l != 10 {
+			t.Errorf("Expected 10 results, got %v", l)
+		}
+
+		if !body.Ok {
+			t.Errorf("Expected OK to be true, got %v", body.Ok)
+		}
+
+		if body.CandidateCount != 21663 {
+			t.Errorf(
+				"Expected candidate_count to be 21663, got %v",
+				body.CandidateCount)
+		}
 	}
 }
 
