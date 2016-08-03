7903963c1d527ed4ef0d12d06ecc42ba26228e77
Nathan Wittstock <nate@milkandtang.com>
2016-08-02 22:38:25 -0700

    Additional tests: request, assert (#3)
    * add request tests
    
    * add assert tests, docs

--- request_test.go
+++ request_test.go
@@ -1,6 +1,10 @@
 package congruent
 
 import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"sort"
 	"testing"
 )
 
@@ -36,5 +40,72 @@ func TestPrepareBodyJSON(t *testing.T) {
 
 	if string(buf.Bytes()) != expected {
 		t.Errorf("expected %s, got %s", expected, buf.Bytes())
+	}
+}
+
+func TestRequest(t *testing.T) {
+	// channel for storing "has been called" ints from test servers
+	c := make(chan int, 2)
+
+	ts0 := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "a")
+		// check header
+		if h := r.Header.Get("X-Test"); h != "zero" {
+			t.Errorf(`Expected header value "zero", got %s`, h)
+		}
+		// write to channel to track request
+		c <- 0
+	}))
+	defer ts0.Close()
+	ts1 := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "b")
+		// check header
+		if h := r.Header.Get("X-Test"); h != "one" {
+			t.Errorf(`Expected header value "one", got %s`, h)
+		}
+		// write to channel to track request
+		c <- 1
+	}))
+	defer ts1.Close()
+
+	servers := Servers([]*Server{
+		NewServer(ts0.URL, &http.Header{"X-Test": []string{"zero"}}),
+		NewServer(ts1.URL, &http.Header{"X-Test": []string{"one"}})})
+
+	request := NewRequest("GET", "/", nil, nil)
+
+	responses, err := servers.Request(request)
+	if err != nil {
+		t.Error(err)
+		t.Fail()
+	}
+
+	// block until both requests finish, and save results
+	var seen []int
+	for i := 0; i < 2; i++ {
+		seen = append(seen, <-c)
+	}
+
+	// verify that we saw responses from both servers
+	sort.Sort(sort.IntSlice(seen))
+	for i, n := range seen {
+		if i != n {
+			t.Errorf("Expected server %d to be called, saw %d", i, n)
+		}
+	}
+
+	// verify we saw responses from each test server
+	var resp []string
+	for _, response := range responses {
+		resp = append(resp, string(response.Body))
+	}
+
+	// verify that we saw responses from both servers
+	sort.Sort(sort.StringSlice(resp))
+	if resp[0] != "a" {
+		t.Errorf(`Expected "a", got %s`, resp[0])
+	}
+	if resp[1] != "b" {
+		t.Errorf(`Expected "b", got %s`, resp[1])
 	}
 }
