edb292fca94001e7e7c58c8554f7b7b5c1670ef8
fardog <nate@milkandtang.com>
2016-08-04 18:13:06 -0700

    add BodyContentSame, tests. update README

--- assert_test.go
+++ assert_test.go
@@ -91,3 +91,46 @@ func TestBodySame(t *testing.T) {
 		t.Error("Expected error, but got none!")
 	}
 }
+
+func TestBodyContentSame(t *testing.T) {
+	gu, err := url.Parse("http://localhost/")
+	if err != nil {
+		t.Error(err)
+		t.Fail()
+	}
+	bu, err := url.Parse("http://bad/")
+	if err != nil {
+		t.Error(err)
+		t.Fail()
+	}
+
+	gs1 := []byte(`{"a":{"b":"c"}}`)
+	gs2 := []byte(`
+    {
+      "a": {
+        "b": "c"
+      }
+    }`)
+	bs1 := []byte(`{"a":{"b":"d"}}`)
+
+	mockReq := &http.Request{Method: "GET", URL: gu}
+	mockBadReq := &http.Request{Method: "GET", URL: bu}
+
+	responses := Responses{
+		&Response{mockReq, nil, gs1, 200},
+		&Response{mockReq, nil, gs2, 200},
+		&Response{mockReq, nil, gs1, 200}}
+
+	if err := responses.BodyContentSame(); err != nil {
+		t.Error(err)
+	}
+
+	responses = Responses{
+		&Response{mockReq, nil, gs2, 200},
+		&Response{mockBadReq, nil, bs1, 200},
+		&Response{mockReq, nil, gs2, 200}}
+
+	if err := responses.BodyContentSame(); err == nil {
+		t.Error("Expected error, but got none!")
+	}
+}
